6a029e31c3c7cb00b9a91a84183244ead55ef508
Bartek <bartek9393@poczta.onet.pl>
2017-07-12 21:37:55 +0200

    Added clear()

--- src/game/Player.java
+++ src/game/Player.java
@@ -5,4 +5,8 @@ public class Player extends Character {
     public Player(int x, int y) {
         super(x, y);
     }
+
+    public void clear(char[][] array) {
+        this.clearCharacter(array);
+    }
 }
