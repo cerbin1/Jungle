9a5b97493c847fe673aa45fb90164739884979c9
Bartek <bartek9393@poczta.onet.pl>
2017-07-12 14:39:57 +0200

    Moved fillBoardWithStars() to Board

--- src/game/Application.java
+++ src/game/Application.java
@@ -3,7 +3,6 @@ package game;
 public class Application {
     public static void main(String[] args) {
         Board board = new Board(10, 20);
-        board.fillBoardWithStars();
         board.displayBoard();
     }
 }
