3e4f58894b65c86e64ed370422d8d9d249704b66
Bartek <bartek9393@poczta.onet.pl>
2017-07-12 15:41:59 +0200

    Added getting moves from user

--- src/game/Application.java
+++ src/game/Application.java
@@ -1,8 +1,35 @@
 package game;
 
+import java.io.IOException;
+import java.lang.*;
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
 public class Application {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException, InterruptedException {
         Board board = new Board(10, 20);
         board.displayBoard();
+        while (true) {
+            System.out.println("wasd to move, q to exit");
+            Scanner scanner = new Scanner(System.in);
+            try {
+                char userInput = scanner.next().charAt(0);
+                if (userInput == 'q') {
+                    System.out.println("Quiting game...");
+                    break;
+                }
+                if (isProperlyMove(userInput)) {
+                    // input processing
+                } else {
+                    System.out.println("Wrong move!");
+                }
+            } catch (InputMismatchException e) {
+                System.out.println(e.getMessage());
+            }
+        }
+    }
+
+    private static boolean isProperlyMove(java.lang.Character input) {
+        return input == 'w' || input == 'a' || input == 's' || input == 'd';
     }
 }
