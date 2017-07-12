dd96290d51051dc8295e60c70565eca6fb97d8aa
Bartek <bartek9393@poczta.onet.pl>
2017-07-12 14:13:39 +0200

    Added class Board, moved board settings to Board from Application

---
+++ src/Board.java
@@ -0,0 +1,27 @@
+public class Board {
+    private int width, height;
+    private char[][] array;
+
+    public Board(int width, int height) {
+        this.width = width;
+        this.height = height;
+        array = new char[width][height];
+    }
+
+    public void fillBoardWithStars() {
+        for (int i = 0; i < height; i++) {
+            for (int j = 0; j < width; j++) {
+                array[j][i] = '*';
+            }
+        }
+    }
+
+    public void displayBoard() {
+        for (int i = 0; i < height; i++) {
+            for (int j = 0; j < width; j++) {
+                System.out.print(array[j][i]);
+            }
+            System.out.println();
+        }
+    }
+}
