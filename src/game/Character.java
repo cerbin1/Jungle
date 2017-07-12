dab48e286541e468ff6c4299c6069da3ab5a58b5
Bartek <bartek9393@poczta.onet.pl>
2017-07-12 14:39:28 +0200

    Added coordinates x and y to Character

--- src/game/Character.java
+++ src/game/Character.java
@@ -1,9 +1,22 @@
 package game;
 
 public abstract class Character {
-    private int strength = 0;
+    private int strength = 0, x, y;
+
+    protected Character(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
 
     public int getStrength() {
         return strength;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
     }
 }
