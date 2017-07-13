72ba7e47a5632ed371a75c52d743eaf6a94ebc5b
Bartek <bartek9393@poczta.onet.pl>
2017-07-13 22:33:42 +0200

    Added test resetStrength()

---
+++ test/game/PlayerTest.java
@@ -0,0 +1,22 @@
+package game;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class PlayerTest {
+    @Test
+    public void shouldResetPlayerStrength() {
+        // given
+        Player player = new Player(0, 0);
+        player.setStrength(15);
+        int strengthBefore = player.getStrength();
+
+        // when
+        player.resetStrength();
+
+        // then
+        assertEquals(15, strengthBefore);
+        assertEquals(0, player.getStrength());
+    }
+}
